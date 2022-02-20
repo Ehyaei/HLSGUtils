dc8ab5d13b0b257ce482ce5971945e635719a1f7
Ahmad <ahmad.ehyaei@gmail.com>
2022-02-20 19:50:18 +0100

    update functions

--- R/parallel_rscripts.r
+++ R/parallel_rscripts.r
@@ -62,22 +62,21 @@ parallel_rscripts <- function(
     swap_memory <- 100*(psutil$swap_memory()["used"]/(swap*1024*1024*1024))
 
 
-    while(virtual_memory > free_memory_treshold  || cpu_percent > free_cpu_treshold){
-
+    while(virtual_memory > free_memory_treshold  | cpu_percent > free_cpu_treshold){
       # ----------------------------
       # If Swap is filled Kill all Process
       if(swap_memory > 95){system("pkill -u mshoai")}
-
       # ----------------------------
       # Check system stats
       cpu_percent <- psutil$cpu_percent(interval=2)
       virtual_memory <- psutil$virtual_memory()["percent"]
       swap_memory <- 100*(psutil$swap_memory()["used"]/(swap*1024*1024*1024))
-
       # ----------------------------
       # every 10 second check system
+      print(k); k = k+1
       Sys.sleep(10)
     }
+
     print(paste("CPU:",cpu_percent, "Memory", virtual_memory))
 
     # Add start, end and simulation name to R Script
